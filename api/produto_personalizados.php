aefea6487b0a5c96d5db82e98a09ef4ba006b99f
Isabela M. Ribeiro <isabela10014@hotmail.com>
2024-11-01 12:24:58 -0300

    feat: add pagina de cestas personalizadas

---
+++ api/produto_personalizados.php
@@ -0,0 +1,50 @@
+<?php
+add_action('rest_api_init', function () {
+    register_rest_route('v1', '/produto', [
+        'methods' => 'POST',
+        'callback' => 'produto_personalizado_rest',
+        'permission_callback' => '__return_true',
+    ]);
+});
+
+function produto_personalizado_rest($request)
+{
+    // Obter todos os dados do corpo da requisição
+    $body = json_decode($request->get_body(), true);
+    
+    // Se precisar enviar os dados para o RD Station
+    $result = produto_personalizado_rd_station($body);
+
+    return rest_ensure_response($result);
+}
+
+function produto_personalizado_rd_station($data)
+{
+    $url = 'https://crm.rdstation.com/api/v1/deals?token=' . RD_STATION_TOKEN;
+
+    // Enviar a requisição
+    $response = wp_remote_post($url, [
+        'method' => 'POST',
+        'headers' => [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+        ],
+        'body' => json_encode($data),
+    ]);
+
+    // Verificar se houve erro na requisição
+    if (is_wp_error($response)) {
+        return ['success' => false, 'message' => 'Erro ao conectar com o SAC'];
+    }
+
+    // Obter o status e o corpo da resposta
+    $status_code = wp_remote_retrieve_response_code($response);
+    $response_body = wp_remote_retrieve_body($response);
+
+    // Retornar o resultado com base no status da resposta
+    if ($status_code === 200) {
+        return ['success' => true, 'message' => 'Contato enviado com sucesso!'];
+    } else {
+        return ['success' => false, 'message' => 'Falha ao enviar o contato: ' . $response_body];
+    }
+}
--- page-personalizadas.php
+++ page-personalizadas.php
@@ -16,7 +16,7 @@ get_header();
         </div>
         <hr class="divisor" />
         <h2>{{ titulo_produtos }}</h2>
-        <form @submit.prevent="validarFormulario">
+        <form @submit.prevent="POSTEntrarEmContato">
           <div class="lista">
             <label class="list" v-for="(item, index) in itens" :key="index">
               <input type="checkbox" :value="item" v-model="itensSelecionados">
@@ -31,7 +31,12 @@ get_header();
                 <span v-if="erros.nomeEmpresa">{{ mensagensErro.nomeEmpresa }}</span>
               </label>
               <label>
-                <input type="text" v-model="cpfCnpj" placeholder="CPF ou CNPJ*" :class="{'erro': erros.cpfCnpj}">
+                <the-mask
+                  v-model="cpfCnpj"
+                  :mask="['###.###.###-##', '##.###.###/####-##']"
+                  placeholder="CPF ou CNPJ"
+                  :class="{'erro': erros.cpfCnpj}">
+                </the-mask>
                 <span v-if="erros.cpfCnpj">{{ mensagensErro.cpfCnpj }}</span>
               </label>
             </div>
@@ -41,65 +46,149 @@ get_header();
                 <span v-if="erros.email">{{ mensagensErro.email }}</span>
               </label>
               <label>
-                <input type="text" v-model="whatsapp" placeholder="WhatsApp*" :class="{'erro': erros.whatsapp}">
+                <the-mask
+                  mask="(##) #####-####"
+                  v-model="whatsapp"
+                  id="telefone_whatsapp"
+                  placeholder="Telefone/Whatsapp"
+                  :class="{'erro': erros.whatsapp}">
+                </the-mask>
                 <span v-if="erros.whatsapp">{{ mensagensErro.whatsapp }}</span>
               </label>
               <label>
-                <input type="text" v-model="quantidade" placeholder="Quantidade*" :class="{'erro': erros.quantidade}">
+                <input type="number" v-model="quantidade" placeholder="Quantidade*" :class="{'erro': erros.quantidade}">
                 <span v-if="erros.quantidade">{{ mensagensErro.quantidade }}</span>
               </label>
             </div>
           </div>
           <div class="item-center">
-            <button type="submit">Enviar</button>
+            <button type="submit" :disabled="isLoading">
+              <span v-if="!isLoading">Enviar</span>
+              <span v-else>Enviando...</span>
+            </button>
           </div>
         </form>
       </div>
+      <div v-if="modalMessage" class="modal">
+        <div class="modal-content">
+          <p>{{ modalMessage }}</p>
+          <div class="item-center">
+            <button @click="closeModal" aria-label="Fechar">Fechar</button>
+          </div>
+        </div>
+      </div>
     </main>
-
-    <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
-    <script>
-      new Vue({
-        el: '#app',
-        data: {
-          imagem: '<?php echo CFS()->get('imagem'); ?>',
-          titulo: '<?php echo esc_html(CFS()->get('titulo')); ?>',
-          texto: `<?php echo wp_kses_post(CFS()->get('texto')); ?>`,
-          titulo_produtos: '<?php echo esc_html(CFS()->get('titulo_produtos')); ?>',
-          itens: <?php echo json_encode(array_column(CFS()->get('itens'), 'item')); ?>,
-          itensSelecionados: [],
-          nomeEmpresa: '',
-          cpfCnpj: '',
-          email: '',
-          whatsapp: '',
-          quantidade: '',
-          erros: {},
-          mensagensErro: {
-            nomeEmpresa: 'Por favor, insira o nome da empresa.',
-            cpfCnpj: 'Por favor, insira o CPF ou CNPJ.',
-            email: 'Por favor, insira um e-mail válido.',
-            whatsapp: 'Por favor, insira o número do WhatsApp.',
-            quantidade: 'Por favor, insira uma quantidade válida.'
-          }
-        },
-        methods: {
-          validarFormulario() {
-            this.erros = {};
-            if (!this.nomeEmpresa) this.erros.nomeEmpresa = true;
-            if (!this.cpfCnpj) this.erros.cpfCnpj = true;
-            if (!this.validarEmail(this.email)) this.erros.email = true;
-            if (!this.whatsapp) this.erros.whatsapp = true;
-            if (!this.quantidade || isNaN(this.quantidade)) this.erros.quantidade = true;
-
-            if (!Object.keys(this.erros).length) alert('Formulário enviado com sucesso!');
-          },
-          validarEmail(email) {
-            return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-          }
-        }
-      });
-    </script>
 
 <?php endwhile;
 endif; ?>
 <?php get_footer(); ?>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/lib/vue-the-mask.min.js"></script>
+
+<script>
+  new Vue({
+    el: '#app',
+    data: {
+      imagem: '<?php echo CFS()->get('imagem'); ?>',
+      titulo: '<?php echo esc_html(CFS()->get('titulo')); ?>',
+      texto: `<?php echo wp_kses_post(CFS()->get('texto')); ?>`,
+      titulo_produtos: '<?php echo esc_html(CFS()->get('titulo_produtos')); ?>',
+      itens: <?php echo json_encode(array_column(CFS()->get('itens'), 'item')); ?>,
+      itensSelecionados: [],
+      nomeEmpresa: '',
+      cpfCnpj: '',
+      email: '',
+      whatsapp: '',
+      quantidade: '',
+      erros: {},
+      isLoading: false,
+      modalMessage: '',
+      mensagensErro: {
+        nomeEmpresa: 'Por favor, insira o nome da empresa.',
+        cpfCnpj: 'Por favor, insira o CPF ou CNPJ.',
+        email: 'Por favor, insira um e-mail válido.',
+        whatsapp: 'Por favor, insira o número do WhatsApp.',
+        quantidade: 'Por favor, insira uma quantidade válida.'
+      }
+    },
+    methods: {
+      validarFormulario() {
+        this.erros = {};
+        if (!this.nomeEmpresa) this.erros.nomeEmpresa = true;
+        if (!this.cpfCnpj) this.erros.cpfCnpj = true;
+        if (!this.validarEmail(this.email)) this.erros.email = true;
+        if (!this.whatsapp) this.erros.whatsapp = true;
+        if (!this.quantidade || isNaN(this.limparFormatoPreco(this.quantidade))) this.erros.quantidade = true;
+
+        return !Object.keys(this.erros).length;
+      },
+      validarEmail(email) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+      },
+      limparFormatoPreco(preco) {
+        return parseFloat(preco.replace(/[^\d,]/g, '').replace(',', '.'));
+      },
+      async POSTEntrarEmContato() {
+        if (!this.validarFormulario()) return;
+        this.isLoading = true;
+
+        const precoLimpo = this.limparFormatoPreco(this.quantidade);
+
+        const data = {
+          deal: {
+            name: this.nomeEmpresa
+          },
+          contacts: [{
+            emails: [{
+              email: this.email
+            }],
+            phones: [{
+              phone: this.whatsapp,
+              type: 'celular'
+            }],
+            title: this.cpfCnpj
+          }],
+          deal_products: [{
+            name: this.nomeEmpresa,
+            price: precoLimpo,
+            total: precoLimpo,
+            description: this.itensSelecionados.join(', ')
+          }]
+        };
+
+        try {
+          const response = await fetch('/wp-json/v1/produto', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+          });
+
+          if (response.ok) {
+            this.modalMessage = "Mensagem enviada com sucesso!";
+            this.limparFormulario(); // Chama a função para limpar o formulário
+          } else {
+            const errorData = await response.json();
+            this.modalMessage = errorData.message || "Erro ao enviar a mensagem.";
+          }
+        } catch (error) {
+          this.modalMessage = "Erro ao enviar a mensagem.";
+        } finally {
+          this.isLoading = false;
+        }
+      },
+      limparFormulario() {
+        this.nomeEmpresa = '';
+        this.cpfCnpj = '';
+        this.email = '';
+        this.whatsapp = '';
+        this.quantidade = '';
+        this.itensSelecionados = [];
+        this.erros = {};
+      },
+      closeModal() {
+        this.modalMessage = '';
+      }
+    }
+  });
+</script>
